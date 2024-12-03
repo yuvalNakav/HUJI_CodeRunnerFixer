5150f2ba557dbb5c7cf1ed8deebfdefdc72c9c4a
yuvalNakav <yuvalnakav1@gmail.com>
2024-12-03 21:34:51 +0200

    init commit. see you in moodle 25-26

---
+++ scripts/fixCodeRunner.js
@@ -0,0 +1,10 @@
+const mainContent = document.getElementById("page-content");
+const quizNav = document.getElementById("theme_osiris-drawers-blocks");
+const asideRect = quizNav && document.getElementById("theme_osiris-drawers-blocks")?.getBoundingClientRect();
+if (mainContent && asideRect?.left) {
+  mainContent.style.position = "absolute";
+  mainContent.style.right = asideRect.left.toString();
+  mainContent.style.left = "0";
+  mainContent.style.width = `${asideRect.left}px`;
+  console.log("code runner fixed");
+} else console.log("im dumb:(");
