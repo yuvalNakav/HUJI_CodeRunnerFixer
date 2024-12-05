c687d88e8ea1c9abcfbf4e47d65cf536f20d1eec
yuvalNakav <yuvalnakav1@gmail.com>
2024-12-05 17:34:11 +0200

    added a revert button and its functionality, includeing terminating eventlisteners

--- scripts/fixCodeRunner.js
+++ scripts/fixCodeRunner.js
@@ -1,14 +1,70 @@
+let isActive = true;
+
+const sleep = (milliseconds) => {
+  return new Promise((resolve) => setTimeout(resolve, milliseconds));
+};
+
+/**
+ * @param {HTMLElement} mainContent
+ */
+function resetContentLayout(mainContent) {
+  const topScrollRect = document.getElementById("topofscroll")?.getBoundingClientRect();
+  console.log("🚀 ~ resetContentLayout ~ topScrollRect:", topScrollRect);
+  if (mainContent) {
+    console.log("all good?");
+    mainContent.style.position = null;
+    mainContent.style.width = null;
+  }
+}
+
+/**
+ * @param {HTMLElement} mainContent
+//  * @param {CSSStyleDeclaration} width
+ */
+async function addRevertButton({ mainContent, width }) {
+  window.addEventListener("resize", fixLayout);
+  const button = document.createElement("button");
+  button.onclick = () => {
+    if (isActive) {
+      resetContentLayout(mainContent);
+      window.removeEventListener("resize", fixLayout);
+    } else {
+      fixLayout();
+      window.addEventListener("resize", fixLayout);
+    }
+    isActive = !isActive;
+  };
+  button.textContent = "Resize Runner";
+  button.id = "fixer-revert-btn";
+  const rowDiv = document.getElementsByClassName("row")[0];
+  rowDiv.append(button);
+
+  button.style.borderRadius = ".25rem";
+  button.style.backgroundColor = "#005f9f";
+  button.style.color = "#FFF";
+  button.style.margin = "0px";
+  button.style.padding = "0px";
+  button.style.height = "40px";
+
+  for (let i = 0; i < 3; i++) {
+    await sleep(300);
+    button.style.opacity = "50%";
+    await sleep(300);
+    button.style.opacity = "100%";
+  }
+}
+
 function fixLayout() {
   const mainContent = document.getElementById("page-content");
   const quizNav = document.getElementById("theme_osiris-drawers-blocks");
+  const contentRect = mainContent.getBoundingClientRect();
+  const { width } = contentRect;
   const asideRect = quizNav && document.getElementById("theme_osiris-drawers-blocks")?.getBoundingClientRect();
   if (mainContent && asideRect?.left) {
     mainContent.style.position = "absolute";
-    mainContent.style.right = asideRect.left.toString();
     mainContent.style.left = "0";
     mainContent.style.width = `${asideRect.left}px`;
-    console.log("code runner fixed");
+    return { mainContent, width };
   } else console.log("im dumb:(");
 }
-window.addEventListener("resize", fixLayout);
-fixLayout();
+addRevertButton(fixLayout());
