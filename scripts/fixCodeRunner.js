c8bf56782fb5766f9cc88b169912c80e1e1774aa
yuvalNakav <yuvalnakav1@gmail.com>
2024-12-10 18:54:37 +0200

    init

--- scripts/fixCodeRunner.js
+++ scripts/fixCodeRunner.js
@@ -19,7 +19,6 @@ function resetContentLayout(mainContent) {
 
 /**
  * @param {HTMLElement} mainContent
-//  * @param {CSSStyleDeclaration} width
  */
 async function addRevertButton({ mainContent, width }) {
   window.addEventListener("resize", fixLayout);
@@ -53,8 +52,55 @@ async function addRevertButton({ mainContent, width }) {
     button.style.opacity = "100%";
   }
 }
+/**
+ * @param {HTMLElement} editor
+ * @param {string} action
+ */
+function adjustFontSize(action, editor) {
+  const fontSize = Number(editor.style.fontSize.replace(/\D/g, ""));
+  newSize = action === "+" ? `${fontSize + 5}px` : `${fontSize - 5}px`;
+  editor.style.fontSize = newSize;
+}
+
+/**
+ * @param {HTMLElement} editor
+ */
+function addFontControls(editor) {
+  const plusButton = document.createElement("button");
+  plusButton.onclick = () => adjustFontSize("+", editor);
+  plusButton.textContent = "+";
+  // plusButton.style.right = "2%"
+  const minusButton = document.createElement("button");
+  minusButton.onclick = () => adjustFontSize("-", editor);
+  minusButton.textContent = "-";
+  // plusButton.style.right = "5%"
+  const buttons = [minusButton, plusButton];
+  const buttonDiv = document.createElement("div");
+  buttonDiv.append(buttons);
+  buttons.forEach((button) => {
+    button.style.borderRadius = ".25rem";
+    button.style.backgroundColor = "#005f9f";
+    button.style.color = "#FFF";
+    button.style.margin = "0px";
+    button.style.padding = "0px";
+    button.style.height = "40px";
+  });
+  editor.append(buttonDiv);
+  // buttonDiv.style.position = "absolute";
+  // buttonDiv.style.top = "0px";
+  // buttonDiv.style.right = "2%";
+  buttonDiv.style.display = "flex";
+  buttonDiv.style.flexDirection = "row";
+}
 
 function fixLayout() {
+  const editors = document.querySelectorAll(".ace_content");
+  console.log("🚀 ~ fixLayout ~ editors:", editors.length);
+
+  for (let i = 0; i < editors.length; i++) {
+    console.log("hi!!!");
+    addFontControls(editors[i]);
+  }
   const mainContent = document.getElementById("page-content");
   const quizNav = document.getElementById("theme_osiris-drawers-blocks");
   const contentRect = mainContent.getBoundingClientRect();
@@ -68,3 +114,7 @@ function fixLayout() {
   } else console.log("im dumb:(");
 }
 addRevertButton(fixLayout());
+
+document.addEventListener(onkeydown, (ev) => {
+  if (ev.ctrlKey && ev.key === "Enter") console.log("correct bind");
+});
